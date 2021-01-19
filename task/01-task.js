590373884d4259f48070df0800070ddf6716d3a1
Chivova <Chivovaz@ua.fm>
2021-01-19 16:31:28 +0200

    add 03-task.js, and update

--- task/01-task.js
+++ task/01-task.js
@@ -17,3 +17,34 @@ const user = {
   hobby: 'html',
   premium: true,
 };
+// console.log(user); 
+// не можно увидеть старый результат, ибо обьект передается по ссылке для этого можно использовать JSON.stringify()
+console.log(JSON.stringify(user)); // ---> метод который переобразовует в строку
+
+user.mood = 'happy';
+// user['mood'] = 'happy';
+
+user.hobby = 'skydiving';
+// user['hobby'] = 'skydiving';
+
+user.premium = false;
+// user['premium'] = false;
+console.dir(user);
+
+const keys = Object.keys(user);
+for (const key of keys) {
+  console.log(`${key}: ${user[key]}`);
+}
+
+// const entries = Object.entries(user);
+// console.log(entries);
+// /*
+//  * На каждой итерации по entries выберем первый элемент подмассива (ключ)
+//  * в переменную key, а второй (значение) в переменную value
+//  */
+// for (const entry of entries) {
+//   const key = entry[0];
+//   const value = entry[1];
+
+//   console.log(`${key}: ${value}`);
+// }
---
+++ task/03-task.js
@@ -0,0 +1,58 @@
+/* Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников
+и возвращает имя самого продуктивного(который выполнил больше всех задач).
+Сотрудники и кол - во выполненых задач содержатся как свойства объекта в формате "имя": "кол-во задач". */
+
+// const findBestEmployee = function (employees) {
+//   for (const employe in employees) {
+//     if (employees[employe] === Math.max(...Object.values(employees))) {
+//       return employe;
+
+//     }
+//   }
+// };
+
+const findBestEmployee = function(employees) {
+  const keys = Object.keys(employees);
+  let bestProductive = keys[0];
+
+  for (let i = 0; i < keys.length; i += 1) {
+    //  console.log(bestEmployee[i]);
+    // console.log(Math.max(employees[bestEmployee[i]]));
+    if (employees[keys[i]] > employees[bestProductive]) {
+       
+      bestProductive = keys[i];
+     
+    }
+      
+  }
+  return bestProductive;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
