4af3f79929e45d814c40c3f5a342a8f22de58e2e
Chivova <Chivovaz@ua.fm>
2021-01-29 15:21:00 +0200

    finished 06-task.js, add and update 07-task.js

--- other-tasks/06-task.js
+++ other-tasks/06-task.js
@@ -6,16 +6,29 @@
 // и false - если не совпадает.
 
 // function savePassword(password) {
-
 //   function checkPassword(str) {
-//     // return password === str ? true : false;
 //     // if (password === str) {
 //     //   return true;
 //     // } else {
 //     //   return false;
 //     // }
+//     // return password === str ? true : false; сокращаенный вариант
+//     // еще более сокращенный, по результату все равно выведет true || false
 //     return password === str;
 //   }
 
 //   return checkPassword;
 // }
+
+// самая краткая запись этой функции:
+const savePassword = (password) => (str) => password === str; // стрелочная фугкция возвращает стрелочную
+
+const chekUserPass = savePassword("qwert"); // здесь функция запоминает пароль. В самой функции запись будет выглядить
+// что-то вроде const password = 'qwert';
+
+console.log(chekUserPass(prompt("Input password:")));
+
+// можно через функцию написать еще одну проверку, например проперку пароля админа
+const chekAdminPass = savePassword("Qwert12345");
+
+console.log(chekAdminPass(prompt("Input password:")));
---
+++ other-tasks/07-task.js
@@ -0,0 +1,31 @@
+// Пример 7 - Замыкания
+// Напишите функцию для хранения скидки.
+// Функция возвращает другую функцию,
+// которая принимает сумму покупки
+// и возвращает финальную сумму с сохранённой скидкой.
+
+const applyDiscount = (discount) => {
+  const getPriceWithDiscount = (price) => {
+    const priceWithDiscount = price - (price * discount) / 100;
+
+    return priceWithDiscount;
+  };
+
+  return getPriceWithDiscount;
+};
+
+// Более краткая запись
+// const applyDiscount = discount => price => price - price * discount / 100;
+
+const withBaseDiscount = applyDiscount(0); // price => price - price * 0 / 100;
+const withSilverDiscount = applyDiscount(5); // price => price - price * 5 / 100;
+const withGoldDiscount = applyDiscount(10); // price => price - price * 10 / 100;
+
+console.log("withBaseDiscount:", withBaseDiscount(100));
+console.log("withBaseDiscount:", withBaseDiscount(200));
+console.log("withSilverDiscount:", withSilverDiscount(100));
+console.log("withSilverDiscount:", withSilverDiscount(200));
+console.log("withGoldDiscount:", withGoldDiscount(100));
+console.log("withGoldDiscount:", withGoldDiscount(200));
+console.log("withGoldDiscount:", withGoldDiscount(300));
+console.log("withGoldDiscount:", withGoldDiscount(400));
