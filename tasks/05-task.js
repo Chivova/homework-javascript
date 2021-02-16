0d2d19b8179f3f370c5c053b7b601b5b5c2485ce
Chivova <Chivovaz@ua.fm>
2021-02-16 10:33:15 +0200

    update and finished 05-task.js

--- tasks/05-task.js
+++ tasks/05-task.js
@@ -5,7 +5,9 @@ class Car {
    * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
    */
   static getSpecs(car) {
-    console.log(car);
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+    );
   }
   /*
    * Конструктор получает объект настроек.
@@ -17,7 +19,7 @@ class Car {
    *  isOn - заведен ли автомобиль, значения true или false. Изначально false
    *  distance - общий киллометраж, изначально 0
    */
-  constructor({ speed = 0, isOn = false, distance = 0, maxSpeed, price } = {}) {
+  constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 } = {}) {
     this.speed = speed;
     this.isOn = isOn;
     this.distance = distance;
@@ -60,32 +62,44 @@ class Car {
    * при условии что результирующая скорость
    * не больше чем значение свойства maxSpeed
    */
-  accelerate(value) {}
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    }
+  }
 
   /*
    * Отнимает от свойства speed полученное значение,
    * при условии что результирующая скорость не меньше нуля
    */
-  decelerate(value) {}
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    }
+  }
 
   /*
    * Добавляет в поле distance киллометраж (hours * speed),
    * но только в том случае если машина заведена!
    */
-  drive(hours) {}
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += hours * this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
 
 mustang.turnOn();
-// mustang.accelerate(50);
-// mustang.drive(2);
+mustang.accelerate(50);
+mustang.drive(2);
 
 Car.getSpecs(mustang);
 // // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
-// mustang.decelerate(20);
-// mustang.drive(1);
+mustang.decelerate(20);
+mustang.drive(1);
 mustang.turnOff();
 
 Car.getSpecs(mustang);
