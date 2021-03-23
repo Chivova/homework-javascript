4929c131534169d8dbd781d314a4e6bae4c4f10d
Chivova <Chivovaz@ua.fm>
2021-03-23 16:05:50 +0200

    update homework-08.js

--- homework-08/js/homework-08.js
+++ homework-08/js/homework-08.js
@@ -2,35 +2,69 @@ import gallery from './data/gallery-items.js';
 
 const refs = {
   galleryList: document.querySelector('.js-gallery'),
+  largeImg: document.querySelector('.lightbox__image'),
 };
-/*
-<li class='gallery__item'>
+
+// function makeItemsGallery({ preview, original, description }) {
+//   const itemGallery = document.createElement('li');
+//   itemGallery.classList.add('gallery__item');
+
+//   const linkGallery = document.createElement('a');
+//   linkGallery.classList.add('gallery__link');
+//   linkGallery.href = `${original}`;
+
+//   const imgGallery = document.createElement('img');
+//   imgGallery.classList.add('gallery__image');
+//   imgGallery.src = `${preview}`;
+//   imgGallery.alt = `${description}`;
+//   imgGallery.dataset.source = `${original}`;
+
+//   linkGallery.append(imgGallery);
+//   itemGallery.append(linkGallery);
+
+//   return itemGallery;
+// }
+
+// const elementsGallery = gallery.map(makeItemsGallery);
+// refs.galleryList.append(...elementsGallery);
+
+function makeItemGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class='gallery__item'>
   <a
     class='gallery__link'
-    href='https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg'
+    href='${original}'
   >
     <img
       class='gallery__image'
-      src='https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg'
-      data-source='https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg'
-      alt='Tulips'
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</li>;
- */
-function createItemsGallery() {
-  const itemGallery = document.createElement('li');
-  itemGallery.classList(gallery__item);
-  const linkGallery = document.createElement('a');
-  linkGallery.classList('gallery__link');
-  linkGallery.href =
-    'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg';
-  const imgGallery = document.createElement('img');
-  imgGallery.classList('gallery__image');
-  imgGallery.src =
-    'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg';
-  imgGallery.alt = 'Tulips';
+</li>`;
+    })
+    .join('');
+}
 
-  imgGallery.datasource =
-    'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg';
+function markupRenderingGallery() {
+  const galleryContainer = makeItemGallery(gallery);
+  refs.galleryList.insertAdjacentHTML('beforeend', galleryContainer);
+}
+markupRenderingGallery();
+
+refs.galleryList.addEventListener('click', onImgOfGalleryClick);
+
+function onImgOfGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains('gallery__image')) return;
+
+  const imgRef = evt.target;
+  setLargeImg(imgRef);
+}
+
+function setLargeImgSrcURL(url) {
+  largeImg.src = url;
 }
